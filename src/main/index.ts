1de02607cb4d28771346fd042f81dc387bc05666
AriSilva94 <ariovaldo.bsjunior@gmail.com>
2026-03-31 17:11:51 -0300

    feat: add main process entry point with window and IPC setup

---
+++ src/main/index.ts
@@ -0,0 +1,51 @@
+import { app, BrowserWindow } from 'electron'
+import { join } from 'path'
+import { initDb } from './db'
+import { registerAllHandlers } from './ipc'
+
+function createWindow(): BrowserWindow {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    minWidth: 900,
+    minHeight: 600,
+    show: false,
+    titleBarStyle: 'default',
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+      sandbox: false,
+      preload: join(__dirname, '../preload/index.js'),
+    },
+  })
+
+  win.on('ready-to-show', () => {
+    win.show()
+  })
+
+  if (process.env['ELECTRON_RENDERER_URL']) {
+    win.loadURL(process.env['ELECTRON_RENDERER_URL'])
+  } else {
+    win.loadFile(join(__dirname, '../renderer/index.html'))
+  }
+
+  return win
+}
+
+app.whenReady().then(async () => {
+  await initDb()
+  registerAllHandlers()
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
