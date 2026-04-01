c67d06ad4dfb5444af226bfb1177db5fa5b7c6a6
AriSilva94 <ariovaldo.bsjunior@gmail.com>
2026-03-31 22:48:21 -0300

    feat: add routes for clients, projects, machines, and operators modules

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,10 +1,16 @@
 import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { Shell } from './components/layout/Shell'
 import { Dashboard } from './pages/Dashboard'
-import { ClientsPage } from './pages/clients'
-import { ProjectsPage } from './pages/projects'
-import { MachinesPage } from './pages/machines'
-import { OperatorsPage } from './pages/operators'
+import { ClientsListPage } from './pages/clients'
+import { ClientFormPage } from './pages/clients/ClientFormPage'
+import { ClientDetailPage } from './pages/clients/ClientDetailPage'
+import { ProjectsListPage } from './pages/projects'
+import { ProjectFormPage } from './pages/projects/ProjectFormPage'
+import { ProjectDetailPage } from './pages/projects/ProjectDetailPage'
+import { MachinesListPage } from './pages/machines'
+import { MachineFormPage } from './pages/machines/MachineFormPage'
+import { OperatorsListPage } from './pages/operators'
+import { OperatorFormPage } from './pages/operators/OperatorFormPage'
 import { DailyLogsPage } from './pages/dailylogs'
 import { CostsPage } from './pages/costs'
 import { RevenuesPage } from './pages/revenues'
@@ -17,10 +23,25 @@ export default function App(): JSX.Element {
         <Route path="/" element={<Shell />}>
           <Route index element={<Navigate to="/dashboard" replace />} />
           <Route path="dashboard" element={<Dashboard />} />
-          <Route path="clients" element={<ClientsPage />} />
-          <Route path="projects" element={<ProjectsPage />} />
-          <Route path="machines" element={<MachinesPage />} />
-          <Route path="operators" element={<OperatorsPage />} />
+
+          <Route path="clients" element={<ClientsListPage />} />
+          <Route path="clients/new" element={<ClientFormPage />} />
+          <Route path="clients/:id" element={<ClientDetailPage />} />
+          <Route path="clients/:id/edit" element={<ClientFormPage />} />
+
+          <Route path="projects" element={<ProjectsListPage />} />
+          <Route path="projects/new" element={<ProjectFormPage />} />
+          <Route path="projects/:id" element={<ProjectDetailPage />} />
+          <Route path="projects/:id/edit" element={<ProjectFormPage />} />
+
+          <Route path="machines" element={<MachinesListPage />} />
+          <Route path="machines/new" element={<MachineFormPage />} />
+          <Route path="machines/:id/edit" element={<MachineFormPage />} />
+
+          <Route path="operators" element={<OperatorsListPage />} />
+          <Route path="operators/new" element={<OperatorFormPage />} />
+          <Route path="operators/:id/edit" element={<OperatorFormPage />} />
+
           <Route path="daily-logs" element={<DailyLogsPage />} />
           <Route path="costs" element={<CostsPage />} />
           <Route path="revenues" element={<RevenuesPage />} />
